ae852e4611bbcb749334e2922fc17495c51879bb
AshVarghese03 <ashvarghese03@gmail.com>
2023-03-14 01:19:09 +0530

    committt

--- chatbot.py
+++ chatbot.py
@@ -4,7 +4,9 @@ from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 import time
 import logging
+
 time.clock= time.time
+
 logger = logging.getLogger() 
 logger.setLevel(logging.CRITICAL)
 
@@ -17,52 +19,59 @@ app = Flask(__name__)
 
 trainer.train(
     [
-        "Hello",
-        "Hi there!",
-        "How are you doing?",
-        "I am doing great",
-        "Can you guide me regarding the admission process?",
-        "Sure!"
-        "Yes sure! Here are some important guidlines you need to follow",
-        "What are the important documents needed?",
-        "Here are the list of important documents you will be needing to complete your admission process?",
-        "● Print out of counselling registration form"
-        "● MHT CET admit card and Result"
-        "● Class 10 and 12 pass certificate and marksheet"
-        "● Category certificate (if applicable)"
-        "● Character and migration certificate"
-        "● School leaving certificate"
-        "● Domicile certificate (if applicable) ",
-        "What happens if candidates commit error in application form?",
-        "In case while filling the application form, if any candidate commits any error in the form or wants to change any entry, then DTE Maharashtra gives a few days"
-        "window to update or edit the details entered in application form. However, this"
-        "facility is just provided only once, so candidates must be very careful while making changes in the application particulars. What needs to be noted is that not all"
-        "the information can be edited or changed with the help of this correction window. ?",
-        "What is Freeze:",
-        "Candidates who are satisfied with their allotted seats and do not want to participate in further have to select this option. By selecting this option candidates will"
-        "confirm their allotted seats and will not be allowed to participate further",
-        "What is Slide:?",
-        "By selecting this option candidates who wish to accept the seat allotted to them"
-        "but will also be open for upgradation into a higher preferred course in the same institute. If in further round, these candidates are allotted in a higher preferred course then"
-        "their previous seat will be cancelled",
-        "What is Float:?", 
-        "This option allows candidates to upgrade their seat to a higher preferred choice"
-        "of course at any institute. However, their earlier allotment will be cancelled if their"
-        "choice of higher course is allotted to them.",
-        "Whats Merit List?",
-        "After the declaration of result, the exam conducting authority releases the provisional"
-        "merit list in online mode. The merit will be prepared based on the marks secured in the"
-        "entrance exam. The provisional merit list carries the name of candidates shortlisted for counselling along with their overall ranks. To check the merit list candidates have to"
-        "log-in into their account by entering application number and date of birth. What needs"
-        "to be noted is that separate merit list is prepared and released for JEE Main qualified"
-        "candidates. MHT CET merit list will be available separately for different groups like all"
-        "India and Maharashtra candidates. A few days window will be given to the candidates"
-        "to send the complaint against the merit list, if they have any. After the feedback, a final"
-        "merit list will be published.", 
+     "Hi",
+"Helloo!",
+"Hey",
+
+"How are you?",
+"I'm good.</br> <br>Go ahead and write the number of any query. 😃✨ <br> 1.list of important documents you will be needing to complete your admission process.</br>2.Frequently asked questions regarding admission </br>3.Brochure of top colleges in Mumbai</br>4.Cut-Off of Different Colleges</br>",
+
+"Great",
+"I'm good.</br> <br>Go ahead and write the number of any query. 😃✨ <br> 1.list of important documents you will be needing to complete your admission process.</br>2.Frequently asked questions regarding admission </br>3.Brochure of top colleges in Mumbai</br>4.Cut-Off of Different Colleges</br>",
+
+"good",
+"I'm good.</br> <br>Go ahead and write the number of any query. 😃✨ <br> 1.list of important documents you will be needing to complete your admission process.</br>2.Frequently asked questions regarding admission </br>3.Brochure of top colleges in Mumbai</br>4.Cut-Off of Different Colleges</br>",
+
+"fine",
+"I'm good.</br> <br>Go ahead and write the number of any query. 😃✨ <br> 1.list of important documents you will be needing to complete your admission process.</br>2.Frequently asked questions regarding admission </br>3.Brochure of top colleges in Mumbai</br>4.Cut-Off of Different Colleges</br>",
+
+"Thank You",
+"Your Welcome 😄",
+
+"Thanks",
+"Your Welcome 😄",
+
+"Bye",
+"Thank You for visiting!..",
+
+"What do you do?",
+"I am made to guide and give you Information about The Admission process",
+
+"What else can you do?",
+"I can help you find colleges and help ypu filling application form",
+    
+    "1",
+     "<b> ● Print out of counselling registration form <br> <br> ● MHT CET admit card and Result <br> ● Class 10 and 12 pass certificate and marksheet <br> ● Category certificate (if applicable) <br> ● Character and migration certificate <br> ● School leaving certificate <br> ● Domicile certificate (if applicable) </b>",
+    
+    "2",
+    "<b> Following are the frequently asked questions during admission <br> <br> 2.1 What is Freeze <br> 2.2 What is Slide <br> 2.3 What is Float <br> 2.4 What is Merit List </b>",
+    
+    "2.1",
+    " <b> What is Freeze : <br> Candidates who are satisfied with their allotted seats and do not want to participate in further have to select this option. <br> By selecting this option candidates will confirm their allotted seats and will not be allowed to participate further </b>",
+    
+    "2.2",
+    " <b> What is Slide : <br> By selecting this option candidates who wish to accept the seat allotted to them but will also be open for upgradation into a higher preferred course in the same institute. <br> If in further round these candidates are allotted in a higher preferred course then their previous seat will be cancelled </b>",
+   
+    "2.3",
+    "<b> What is Float : <br> This option allows candidates to upgrade their seat to a higher preferred choice of course at any institute. <br> However their earlier allotment will be cancelled if their choice of higher course is allotted to them. </b> ",
+  
+    "2.4",
+    "<b>  What is Merit List : <br> After the declaration of result the exam conducting authority releases the provisional merit list in online mode. <br> The merit will be prepared based on the marks secured in the entrance exam. <br> The provisional merit list carries the name of candidates shortlisted for counselling along with their overall ranks. <br> To check the merit list candidates have to login into their account by entering application number and date of birth. <br> What needs to be noted is that separate merit list is prepared and released for JEE Main qualified candidates. <br> MHT CET merit list will be available separately for different groups like all India and Maharashtra candidates. <br> A few days window will be given to the candidates to send the complaint against the merit list if they have any. <br> After the feedback a final merit list will be published. </b> ",
+    
+    "3",
+    "<b> Brochure of top colleges in mumbai : </b> ",
     ]
 )
-
-
 
 
 while True:
